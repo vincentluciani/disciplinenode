81b5a6256382d8455eb6d0fa4aba8decc2ee6d6d
vincent luciani <vincent.luciani.fr@gmail.com>
2023-01-22 08:35:41 +0100

    google validation

--- api/habits.js
+++ api/habits.js
@@ -2,6 +2,27 @@ const express = require('express')
 const router = express.Router()
 const HabitsService = require('../services/HabitsService')
 const common = require('../utils/requestManager')
+const {OAuth2Client} = require('google-auth-library');
+const configManager = require('../utils/configManager.js')
+
+const configuration = configManager.getApplicationConfiguration();
+
+const client = new OAuth2Client(configuration.clientId);
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: configuration.clientId,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  return payload;
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+
+
 
 router.get('/:userId',(request,response)=>{
 
@@ -29,6 +50,31 @@ router.post('/', function(request, response){
 
 });
 
+
+router.post('/auth', function(request, response){
+
+    var body = ''
+    request.on('data', function(data) {
+      body += data
+      console.log('Partial body: ' + body)
+    })
+    request.on('end', function() {
+      console.log('Body: ' + body)
+
+      const token=body.replace(/token=(\w*)/,'$1');
+      verify(token).then(
+        value => {
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end(value.picture)
+        },
+        reason => {
+            console.log(reason)
+        }
+      )
+      
+      
+    })
+});
 
 
 module.exports = router;
