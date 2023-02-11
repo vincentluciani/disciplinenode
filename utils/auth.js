48eface110ba01f7381365d9b6af8a077b90ad82
vincent luciani <vincent.luciani.fr@gmail.com>
2023-02-11 16:21:31 +0100

    application issues its own token

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const HabitsService = require('../services/HabitsService')
 const common = require('../utils/requestManager')
-
+const jwtCreator = require('./jwtManager.js')
 const postReceiver = require('./postReceiver.js')
 const authenticationManager = require('./authenticationManager.js')
 
@@ -11,7 +11,61 @@ router.post('/',postReceiver, authenticationManager.authorizationManager, functi
         /* todo put final handling in a middleware */
         if (request.authentication.isAuthenticated){
             response.writeHead(200, {'Content-Type': 'text/html'})
-            response.end(request.authentication.picture)
+            /* todo: should return our own token */
+            response.end({
+                applicationJwtToken: request.authentication.applicationJwtToken,
+                picture:request.authentication.picture
+            })
+        } else {
+            response.writeHead(401, {'Content-Type': 'text/html'})
+            response.end("Authentication Failed")
+        }
+    })
+
+
+    
+    router.post('/testcreatetoken',postReceiver, function(request, response){
+        /* todo put final handling in a middleware */
+        if (request.body){
+
+            userData={userId:'e434343434'}
+            jwtCreator.jwtCreate(userData).then(
+                value => {
+                    console.log(value)
+                    response.writeHead(200, {'Content-Type': 'text/html'})
+                    response.end(value)
+                },
+                reason => {
+                    console.log(reason)
+                    response.writeHead(401, {'Content-Type': 'text/html'})
+                    response.end(reason)
+                }
+            )
+
+
+        } else {
+            response.writeHead(401, {'Content-Type': 'text/html'})
+            response.end("Authentication Failed")
+        }
+    })
+
+    router.post('/testreceivetoken',postReceiver, function(request, response){
+        /* todo put final handling in a middleware */
+        if (request.body){
+            var token = request.headers['x-access-token']
+
+            jwtCreator.jwtValidate(token).then(
+                value => {
+                    console.log(value)
+                },
+                reason => {
+                    console.log(reason)
+                }
+            )
+
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            /* todo: should return our own token */
+            response.end(request.body)
         } else {
             response.writeHead(401, {'Content-Type': 'text/html'})
             response.end("Authentication Failed")
@@ -19,3 +73,4 @@ router.post('/',postReceiver, authenticationManager.authorizationManager, functi
     })
 
     module.exports = router;
+    
--- utils/authenticationManager.js
+++ utils/authenticationManager.js
@@ -1,3 +1,5 @@
+const jwtManager = require('./jwtManager.js')
+
 const {OAuth2Client} = require('google-auth-library');
 require('./User');
 
@@ -7,7 +9,7 @@ const verificationManager =  (request, result, next) =>{
     //const token=request.body.replace(/token=(\w*)/,'$1');
     const token =  request.body.token
 
-    googleAuthenticate(token, request.configuration, request.mongoose).then(
+    applicationAuthenticate(token, request.configuration, request.mongoose).then(
         value => {
             request.authentication = value
             next();
@@ -28,7 +30,7 @@ const authorizationManager =  (request, result, next) =>{
 
     const token=request.body.replace(/token=(\w*)/,'$1');
     //const token = request.body.token
-
+    
     authenticateAndAddUser(token, request.configuration, request.mongoose).then(
         value => {
             request.authentication = value
@@ -48,10 +50,17 @@ const authorizationManager =  (request, result, next) =>{
 const authenticateAndAddUser = async (token,configuration,mongoose) => {
 
     const authentication = await googleAuthenticate(token, configuration,mongoose)
-
     if (!authentication.applicationUser) {
+        
         authentication.applicationUser  = await addUser(authentication,mongoose)
+
     }
+    
+    const userInformation = {
+        userId:authentication.applicationUser
+    }
+    const applicationJwtToken = await jwtCreate.jwtManager(userInformation)
+    authentication.applicationJwtToken = applicationJwtToken
     
     return authentication
 
@@ -99,6 +108,27 @@ const googleAuthenticate = async (token,configuration,mongoose) => {
 }
 
 
+const applicationAuthenticate = async (token,configuration,mongoose) => {
+
+    const payload = await jwtManager(token);
+
+    const User = mongoose.model('users');
+    //User.findOne({_id: payload.userId})
+
+    let user = await User.findOne({_id: payload.userId})
+
+    if (user){
+        return {
+            isAuthenticated: true,
+            applicationUserId: payload.userId
+        }
+    } else {
+        return {
+            isAuthenticated: false
+        }
+    }
+
+}
 const verify = async (token,configuration) => {
     const client = new OAuth2Client(configuration.clientId);
     const ticket = await client.verifyIdToken({
