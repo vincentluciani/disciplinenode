fc3c63af49916373fa90476a73d792abd1eab0f0
vincent luciani <vincent.luciani.fr@gmail.com>
2023-02-11 16:21:31 +0100

    application issues its own token

---
+++ utils/jwtManager.js
@@ -0,0 +1,43 @@
+
+let jwt = require('jsonwebtoken');
+
+const jwtCreate = async (userData) =>{
+
+    var jwtOptions = {
+        // audience :  ["http://localhost:3000","https://brainstrive","https://www.brainstrive.com"],
+         issuer : "https://www.vince.com",
+         expiresIn : '1d',
+         //iat : nowSeconds
+       };
+
+    let token = jwt.sign(userData, 'test1secret',jwtOptions);
+    console.log("creating token")
+    return token
+}
+
+const jwtValidate = async (token) =>{
+// jwt.verify(token, global.config.secretKey
+    var jwtOptions = {
+        // audience :  ["http://localhost:3000","https://brainstrive","https://www.brainstrive.com"],
+        issuer : "https://www.vince.com",
+        expiresIn : '1d'
+        //iat : nowSeconds
+    };
+    jwt.verify(token, 'test1secret', jwtOptions, function (err, decoded) {
+        if (err) {
+            let errordata = {
+                message: err.message,
+                expiredAt: err.expiredAt
+            }
+            console.log(errordata)
+            return 'Unauthorized Access'
+        }
+
+        console.log(decoded);
+        return decoded;
+    });
+}
+module.exports = {
+    jwtCreate: jwtCreate,
+    jwtValidate:jwtValidate
+  }
