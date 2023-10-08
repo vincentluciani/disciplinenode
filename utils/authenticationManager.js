167c8305cbe08c29361a63af486a7e013cf42d01
Vincent Luciani <vincent.luciani.fr@gmail.com>
2023-10-08 07:45:41 +0200

    login with refresh token

--- utils/authenticationManager.js
+++ utils/authenticationManager.js
@@ -7,8 +7,9 @@ require('./User');
 const verificationManager =  (request, result, next) =>{
 
     //const token=request.body.replace(/token=(\w*)/,'$1');
+    const token = request.cookies.authToken
     const logger = request.lm.logger
-    applicationAuthenticate(request, request.mongoose).then(
+    applicationAuthenticate(token, request).then(
         value => {
             request.authentication = value
             next();
@@ -34,6 +35,7 @@ const verificationManager =  (request, result, next) =>{
 const authorizationManager =  (request, result, next) =>{
 
     const token=request.body.replace(/token=(\w*)/,'$1');
+    
     //const token = request.body.token
     
     authenticateAndAddUser(token, request, request.mongoose).then(
@@ -64,9 +66,14 @@ const authenticateAndAddUser = async (token,request,mongoose) => {
     const userInformation = {
         userId:authentication.applicationUser
     }
-    const applicationJwtToken = await jwtManager.jwtCreate(userInformation,request)
-    authentication.applicationJwtToken = applicationJwtToken
-    
+    const jwtObject = await jwtManager.jwtCreate(userInformation,request)
+    authentication.applicationJwtToken = jwtObject.applicationJwtToken
+    authentication.tokenExpiry = jwtObject.expiration
+    const jwtRefreshObject = await jwtManager.generateRefreshToken(userInformation,request)
+    authentication.applicationRefreshToken = jwtRefreshObject.refreshAuthToken
+    authentication.refreshTokenExpiry = jwtRefreshObject.expiration
+    /* TODO PUT IN A DATABASE INSTEAD OF THE PREVIOUS ENTRY - KEY = ??*/
+        
     return authentication
 
 }
@@ -115,12 +122,13 @@ const googleAuthenticate = async (token,configuration,mongoose) => {
 }
 
 
-const applicationAuthenticate = async (request,mongoose) => {
+const applicationAuthenticate = async (token,request) => {
 
-    const token =  request.body.token
+    //const token =  request.body.token
+    
     const payload = await jwtManager.jwtValidate(token,request)
 
-    const User = mongoose.model('users')
+    const User = request.mongoose.model('users')
     //User.findOne({_id: payload.userId})
 
     let user = await User.findOne({_id: payload.userId})
@@ -150,9 +158,55 @@ const verify = async (token,configuration) => {
     // const domain = payload['hd'];
 }
   
+// Handle refresh token
+// TODO
+const refreshManager =  (request, result, next) =>{
 
+    const refreshToken = request.cookies.refreshToken;
+/* TODO CHECK IF TOKEN ALREADY THERE IN DB AS PARENT OR GRANDPARENT ?- IF YES, DENY as refresh token already used*/
+    handleRefreshToken(refreshToken, request).then(
+        value => {
+            request.authentication = value
+            next()
+        },
+        reason => {
+            request.lm.logger.error(reason)
+            request.authentication = {
+            isAuthenticated: false,
+            picture: ''
+            }
+            next();
+        }
+    )     
+}
+
+const handleRefreshToken = async (token,request) => {
+
+    const authentication = await applicationAuthenticate(token,request)
+    if (authentication) {    
+        const userInformation = {
+            userId:authentication.applicationUserId
+        }
+        const applicationJwtToken = await jwtManager.jwtCreate(userInformation,request)
+        authentication.applicationJwtToken = applicationJwtToken.applicationJwtToken
+        authentication.tokenExpiry = applicationJwtToken.expiration
+        const applicationRefreshToken = await jwtManager.generateRefreshToken(userInformation,request)
+        authentication.applicationRefreshToken = applicationRefreshToken.refreshAuthToken
+        authentication.refreshTokenExpiry = applicationRefreshToken.expiration
+
+        /* TODO PUT IN A DATABASE INSTEAD OF THE PREVIOUS ENTRY - KEY = ??*/
+        
+
+        return authentication
+
+    } else {
+        return null
+    }
+
+}
 
 module.exports = {
     verificationManager: verificationManager,
-    authorizationManager: authorizationManager
+    authorizationManager: authorizationManager,
+    refreshManager
   }
--- utils/jwtManager.js
+++ utils/jwtManager.js
@@ -1,20 +1,53 @@
 
 let jwt = require('jsonwebtoken');
+const crypto = require('crypto')
 
 const jwtCreate = async (userData,request) =>{
-
+    let authentication = {}
     var jwtOptions = {
         // audience :  ["http://localhost:3000","https://brainstrive","https://www.brainstrive.com"],
          issuer : "https://www.vince.com",
-         expiresIn : '1d',
+         expiresIn : '10m',
          //iat : nowSeconds
        };
 
-    let token = jwt.sign(userData, request.configuration.secretKey,jwtOptions);
+    authentication.applicationJwtToken = jwt.sign(userData, request.configuration.secretKey,jwtOptions);
     request.lm.logger.info("creating token")
-    return token
+
+
+    const now = new Date(); // Get the current date and time
+    now.setMinutes(now.getMinutes() + 10); // Add 10 minutes
+
+    authentication.expiration = now
+
+    return authentication
 }
 
+  // Generate a refresh token (custom function)
+  const generateRefreshToken = async (userData,request) => {
+    let authentication = {}
+    const refreshJwtOptions = {
+        issuer: 'https://www.vince.com',
+        expiresIn: '30d', // Set a longer expiration time for the refresh token (e.g., 30 days)
+      };
+    // Generate a secure refresh token (you should use a secure library for this)
+    //const refreshToken = crypto.randomBytes(32).toString('hex');
+
+    // Sign the refresh token as a JWT
+    //const refreshAuthToken = jwt.sign({ refreshToken }, request.configuration.secretKey, refreshJwtOptions);
+    authentication.refreshAuthToken = jwt.sign(userData, request.configuration.secretKey,refreshJwtOptions);
+    
+    const currentDate = new Date();
+    
+    // Add one day (24 hours) to the current date
+    currentDate.setDate(currentDate.getDate() + 30)
+
+    authentication.expiration = currentDate
+
+    return authentication;
+  }
+
+  
 const jwtValidate = async (token,request) =>{
 // jwt.verify(token, global.config.secretKey
     var jwtOptions = {
@@ -50,5 +83,6 @@ const jwtValidate = async (token,request) =>{
 }
 module.exports = {
     jwtCreate: jwtCreate,
-    jwtValidate:jwtValidate
+    jwtValidate:jwtValidate,
+    generateRefreshToken
   }
